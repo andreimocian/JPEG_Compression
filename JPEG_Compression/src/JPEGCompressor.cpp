229087c60b7c09a77e0be3482b4b405fc49b2d63
andreimocian <andrei_mocian@yahoo.com>
2026-05-17 23:03:57 +0300

    forward dct and quantization

--- JPEG_Compression/src/JPEGCompressor.cpp
+++ JPEG_Compression/src/JPEGCompressor.cpp
@@ -9,6 +9,28 @@ JPEGCompressor::JPEGCompressor(Mat_<Vec3b> initial_img)
 	cvtColor(initial_img, ycrcb_img, COLOR_BGR2YCrCb);
 
 	split_Y_Cr_Cb(ycrcb_img);
+
+	this->quantization_table_luminance = {
+		{16, 11, 10, 16, 24, 40, 51, 61},
+		{12, 12, 14, 19, 26, 58, 60, 55},
+		{14, 13, 16, 24, 40, 57, 69, 56},
+		{14, 17, 22, 29, 51, 87, 80, 62},
+		{18, 22, 37, 56, 68, 109, 103, 77},
+		{24, 35, 55, 64, 81, 104, 113, 92},
+		{49, 64, 78, 87, 103, 121, 120, 101},
+		{72, 92, 95, 98, 112, 100, 103, 99}
+	};
+
+	this->quantization_table_chrominance = {
+		{17, 18, 24, 47, 99, 99, 99, 99},
+		{18, 21, 26, 66, 99, 99, 99, 99},
+		{24, 26, 56, 99, 99, 99, 99, 99},
+		{47, 66, 99, 99, 99, 99, 99, 99},
+		{99, 99, 99, 99, 99, 99, 99, 99},
+		{99, 99, 99, 99, 99, 99, 99, 99},
+		{99, 99, 99, 99, 99, 99, 99, 99},
+		{99, 99, 99, 99, 99, 99, 99, 99}
+	};
 }
 
 void JPEGCompressor::split_Y_Cr_Cb(Mat_<Vec3b> initial_img)
@@ -66,6 +88,47 @@ static Mat_<float> convert_to_float(Mat_<uchar> img)
 	return intermidiate;
 }
 
+Mat_<float> JPEGCompressor::f_dct(const Mat_<float>& block_8x8_float)
+{
+	Mat_<float> res(8, 8);
+	for (int u = 0; u < 8; u++)
+	{
+		for (int v = 0; v < 8; v++)
+		{
+			float Cu = (u == 0 ? 1 / sqrt(2) : 1);
+			float Cv = (v == 0 ? 1 / sqrt(2) : 1);
+
+			float sum = 0.0f;
+			for (int x = 0; x < 8; x++)
+			{
+				for (int y = 0; y < 8; y++)
+				{
+					sum += block_8x8_float(x, y) * cos(((2 * x + 1) * u * CV_PI) / 16) * cos(((2 * y + 1) * v * CV_PI) / 16);
+				}
+			}
+			res(u, v) = (1.0f / 4.0f) * Cu * Cv * sum;
+		}
+	}
+
+	return res;
+}
+
+Mat_<int> JPEGCompressor::quantization(const Mat_<float>& dct_res, int channel)
+{
+	Mat_<int> res(8, 8);
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (channel == Y_CHANNEL)
+				res(i, j) = (int)std::round(dct_res(i, j) / this->quantization_table_chrominance[i][j]);
+			else
+				res(i, j) = (int)std::round(dct_res(i, j) / this->quantization_table_luminance[i][j]);
+		}
+	}
+	return res;
+}
+
 void JPEGCompressor::process_blocks()
 {
 	for (int channel = 0; channel < 3; channel++)
@@ -80,15 +143,12 @@ void JPEGCompressor::process_blocks()
 
 				block_8x8_float -= 128.0f;
 
+				Mat_<float> dct_res = f_dct(block_8x8_float);
 
+				Mat_<int> quantized_block = quantization(dct_res, channel);
 			}
 		}
 	}
-}
-
-void JPEGCompressor::f_dct()
-{
-
 }
 
 void JPEGCompressor::compress()
--- JPEG_Compression/src/JPEGCompressor.h
+++ JPEG_Compression/src/JPEGCompressor.h
@@ -17,11 +17,20 @@ private:
 	void split_Y_Cr_Cb(Mat_<Vec3b> initial_img);
 	void image_padding();
 	void process_blocks();
-	void f_dct();
+	Mat_<float> f_dct(const Mat_<float>& block_8x8_float);
+	Mat_<int> quantization(const Mat_<float>& res, int channel);
 
 	std::vector<Mat_<uchar>> channels;
 	std::vector<Mat_<uchar>> padded_channels;
 	int rows, rows_with_padding;
 	int cols, cols_with_padding;
+	std::vector<std::vector<int>> quantization_table_luminance;
+	std::vector<std::vector<int>> quantization_table_chrominance;
+
+	enum {
+		Y_CHANNEL,
+		CR_CHANNEL,
+		CB_CHANNEL
+	};
 };
 
